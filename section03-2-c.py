d406cb06bcba3e4684f6e07a5b630c158d2dda15
Ankiwoong <ankiwoong@gmail.com>
2020-07-19 19:41:46 +0900

    section03-2 복습 - 20.07.19

--- section03-2-c.py
+++ section03-2-c.py
@@ -11,22 +11,32 @@ API = "http://www.mois.go.kr/gpms/view/jsp/rss/rss.jsp"
 
 params = []
 
-for num in [1001, 1012, 1013, 1014]:
-    params.append(dict(ctxCd=num))
+for num in [1001, 1012, 1013, 1014]:  # 글 번호
+    params.append(dict(ctxCd=num))  # 리스트 안에 딕셔너리
+
+# 중간 확인
+# print(params)
 
 # 연속해서 4회 요청
 for c in params:
     # 파라미터 출력
     # print(c)
+
     # URL 인코딩
     param = urllib.parse.urlencode(c)
+    # print(param)
+
     # URL 완성
     url = API + "?" + param
+
     # URL 출력
-    print("url=", url)
+    print("url :", url)
+
     # 요청
     res_data = urllib.request.urlopen(url).read()
+
     # 수신 후 디코딩
     contents = res_data.decode("utf-8")
+
     # 출력
     print(contents)
