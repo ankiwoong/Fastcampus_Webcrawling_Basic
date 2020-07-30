33f2870e5d848c429c5c565efc199d07e1146f1e
Ankiwoong <ankiwoong@gmail.com>
2020-07-30 21:01:32 +0900

    section04-2 복습 - 20.07.30

--- section04-2-c.py
+++ section04-2-c.py
@@ -8,51 +8,51 @@
 import json
 import requests
 
-
 s = requests.Session()
 
 # 100개 Json 데이터 요청
-r = s.get('http://httpbin.org/stream/100', stream=True)
+# r = s.get("http://httpbin.org/stream/100", stream=True)
 
 # 수신 확인
 # print(r.text)
 
 # Encoding 확인
-# print('Encoding : {}'.format(r.encoding))
+# print("Before Encoding : {}".format(r.encoding))
 
 # 데이터 타입 확인
 # print(type(r))
 
 # Encoding 타입 체크 후 UTF-8 변경
 if r.encoding is None:
-    r.encoding = 'UTF-8'
+    r.encoding = "UTF-8"
+
+# print("After Encoding : {}".format(r.encoding))
 
 # 수신 데이터 -> Dict 변환
 for line in r.iter_lines(decode_unicode=True):
     # 라인 출력 후 타입 확인
     # print(line)
     # print(type(line))
-    
+
     # Json(Dict) 변환 후 타입 확인
-    b = json.loads(line)
+    b = json.loads(line)  # str -> dict
     # print(b)
     # print(type(b))
-    
+
     # 정보 내용 출력
     for k, v in b.items():
-        pass
+        # pass
         # print("Key: {}, Values: {}".format(k, v))
     # 줄 바꿈
-    print()
-    print()
+    # print()
+    # print()
 
 
-s.close()
+# s.close()
 
+# r = s.get("https://jsonplaceholder.typicode.com/posts")
 
-# r = s.get('https://jsonplaceholder.typicode.com/posts)
-r = s.get('https://jsonplaceholder.typicode.com/posts/1')
-
+r = s.get("https://jsonplaceholder.typicode.com/posts/1")
 # Header 정보
 print(r.headers)
 # 본문 정보
