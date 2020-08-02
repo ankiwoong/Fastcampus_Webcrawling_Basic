116ace171d60b48f60fc8fb05111d134c71cb40a
Ankiwoong <ankiwoong@gmail.com>
2020-08-02 18:19:07 +0900

    section06-1 복습 - 20.08.03

--- section06-1-c.py
+++ section06-1-c.py
@@ -7,7 +7,7 @@
 from selenium import webdriver
 
 # webdriver 설정(Chrome, Firefox 등)
-browser = webdriver.Chrome('./webdriver/chrome/chromedriver.exe')
+browser = webdriver.Chrome("C:\\chromedriver\\chromedriver.exe")
 
 # 크롬 브라우저 내부 대기
 browser.implicitly_wait(5)
@@ -19,39 +19,44 @@ print(dir(browser))
 browser.set_window_size(1920, 1280)  # maximize_window(), minimize_window()
 
 # 페이지 이동
-browser.get('https://www.daum.net')
+browser.get("https://www.daum.net")
+
 
 # 페이지 내용
-print('Page Contents : {}'.format(browser.page_source))
+print("Page Contents : {}".format(browser.page_source))
 
 print()
 
 # 세션 값 출력
-print('Session ID : {}'.format(browser.session_id))
+print("Session ID : {}".format(browser.session_id))
 
 # 타이틀 출력
-print('Title : {}'.format(browser.title))
+print("Title : {}".format(browser.title))
 
-# 현재 URL
-print('URL : {}'.format(browser.current_url))
+# 현재 URL 출력
+print("URL : {}".format(browser.current_url))
 
-# 쿠키 정보 확인
-print('Cookies : {}'.format(browser.get_cookies()))
+# 현재 쿠키 정보 출력
+print("Cookies : {}".format(browser.get_cookies()))
 
 # 검색창 input 선택
-element = browser.find_element_by_css_selector('div.inner_search > input.tf_keyword')
+element = browser.find_element_by_css_selector("div.inner_search > input.tf_keyword")
 
 # 검색어 입력
-element.send_keys('테스트 검색')
+element.send_keys("방탄소년단")
 
 # 검색(Form Submit)
 element.submit()
 
 # 스크린 샷 저장1
-browser.save_screenshot("c:/website_ch1.png")
+browser.save_screenshot(
+    "D:\\Code\\Study\\Fastcampus_Webcrawling_Basic\\result\\website_ch1.png"
+)
 
 # 스크린 샷 저장2
-browser.get_screenshot_as_file("c:/website_ch2.png")
+browser.get_screenshot_as_file(
+    "D:\\Code\\Study\\Fastcampus_Webcrawling_Basic\\result\\website_ch2.png"
+)
 
 # 브라우저 종료
 browser.quit()
